0f37388a64c151230d4546f704310fcdd5f90eec
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2022-01-09 06:30:47 +0900

    軽微な修正

--- apps/blog-front/utils/domain/index.ts
+++ apps/blog-front/utils/domain/index.ts
@@ -1,7 +1,6 @@
 import path from "path";
 
 export const getUrl = (...paths:string[])=>{
-  const domain = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_VERCEL_URL
-    : process.env.NEXT_PUBLIC_BLOG_FRONT_DOMAIN;
+  const domain = process.env.NODE_ENV === 'production'?process.env.NEXT_PUBLIC_BLOG_FRONT_DOMAIN : process.env.NEXT_PUBLIC_VERCEL_URL;
   return path.join(...[domain,...paths]);
 }
