1abce47e5bd55b7ebab39fddf474f6a0dab67fb0
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2022-01-09 07:16:05 +0900

    SEO

--- apps/blog-front/utils/domain/index.ts
+++ apps/blog-front/utils/domain/index.ts
@@ -2,9 +2,7 @@ import path from "path";
 
 export const getUrl = (...paths:string[])=>{
   const domain = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BLOG_FRONT_DOMAIN : process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_BLOG_FRONT_DOMAIN;
-  console.log({domain})
-  const joinedPath = path.join(...[...paths]).replace('.','');
+  const joinedPath = path.join(...['/',...paths]);
   const res = new URL(joinedPath,domain).href
-  console.log({res})
   return res;
 }
