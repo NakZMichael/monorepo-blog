4298faecfe3f734d5052b336da957776e01e927f
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2022-01-09 06:58:20 +0900

    URLを修正

--- apps/blog-front/utils/domain/index.ts
+++ apps/blog-front/utils/domain/index.ts
@@ -1,6 +1,10 @@
 import path from "path";
 
 export const getUrl = (...paths:string[])=>{
-  const domain = process.env.NODE_ENV === 'production'?process.env.NEXT_PUBLIC_BLOG_FRONT_DOMAIN : process.env.NEXT_PUBLIC_VERCEL_URL;
-  return path.join(...[domain,...paths]);
+  const domain = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BLOG_FRONT_DOMAIN : process.env.NEXT_PUBLIC_VERCEL_URL || process.env.NEXT_PUBLIC_BLOG_FRONT_DOMAIN;
+  console.log({domain})
+  const joinedPath = path.join(...[...paths]).replace('.','');
+  const res = new URL(joinedPath,domain).href
+  console.log({res})
+  return res;
 }
