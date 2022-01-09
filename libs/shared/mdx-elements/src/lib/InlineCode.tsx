3f247fc3ee60213356a4d425f713cb283dab51fa
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2022-01-09 19:41:54 +0900

    update inline code

--- libs/shared/mdx-elements/src/lib/InlineCode.tsx
+++ libs/shared/mdx-elements/src/lib/InlineCode.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Box, Button, styled, Typography } from '@mui/material';
+import { grey } from '@mui/material/colors';
 
 export const InlineCode = ({ children, className, ...props }: {
   children:React.ReactNode,
@@ -7,15 +8,19 @@ export const InlineCode = ({ children, className, ...props }: {
 })=>{
   return (
     <Typography 
-        variant="body1"
-        component="code" 
-        sx={{
-        color:theme=>theme.palette.common.white,
-        backgroundColor:theme=>theme.palette.primary.main,
-        borderRadius:"3px",
-        fontFamily:'monospace',
-        padding:"0px 3px",
-        }}
+      variant="body1"
+      component="code" 
+      // HACK: 何故かsxでfontSizeを指定しても効かない
+      style={{
+        fontSize:'1rem',
+      }}
+      sx={{
+        backgroundColor:grey[200],
+        borderRadius:1,
+        fontFamily: 'monospace',
+        paddingX: '3px',
+        // fontSize:'1rem',
+      }}
     >
         {String(children).replace(/\n$/, '')}
     </Typography>
