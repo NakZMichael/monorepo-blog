40c3661e387822d5a5271303c7dc49050cf96174
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2022-01-09 20:53:00 +0900

    fix layout

---
+++ libs/shared/ui/src/lib/logo/logo.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface LogoProps {}
+
+export function Logo(props: LogoProps) {
+  return (
+    <div>
+      <h1>Welcome to Logo!</h1>
+    </div>
+  );
+}
+
+export default Logo;
