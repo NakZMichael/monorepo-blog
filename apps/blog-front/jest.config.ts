27399bd511adc1427ef2ca81d8d503598985de2f
Muka Nakazato <61081351+NakZMichael@users.noreply.github.com>
2022-07-26 09:32:20 +0900

    nxのバージョンアップ

--- apps/blog-front/jest.config.ts
+++ apps/blog-front/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'blog-front',
   preset: '../../jest.preset.js',
   transform: {
--- libs/shared/mdx-elements/jest.config.ts
+++ libs/shared/mdx-elements/jest.config.ts
@@ -1,4 +1,5 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'shared-mdx-elements',
   preset: '../../../jest.preset.js',
   transform: {
